42200e78a2924e3c0986f3369e57543b60e62204
Donia-F <doniaferjani20@gmail.com>
2022-06-18 19:29:55 +0200

    modificati file signup.scss,adepartments/manager_department.html.erb, departments/new.html.erb, favourite_spaces/index.html.erb, reservations/new.html.erb, reservations/reserved.html.erb e aggiunto file reservations.scss

--- app/models/space.rb
+++ app/models/space.rb
@@ -4,7 +4,7 @@ class Space < ApplicationRecord
     has_many :reservations, dependent: :destroy             #
     has_many :quick_reservations, dependent: :destroy       # Permette la distruzione a cascata di tutte le associazioini collegate
     has_many :favourite_spaces, dependent: :destroy         #
-    has_many :seats, through: :spaces, dependent: :destroy  #
+    has_many :seats, dependent: :destroy  #
 
     accepts_nested_attributes_for :seats                # 
     accepts_nested_attributes_for :reservations         # Permette di aggiornare altre tabelle accedendovi tramite le associazioni
